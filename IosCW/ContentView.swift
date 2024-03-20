d873d2663a5177045f9455b296c4b9f04ad62d75
@SandunVidusankha <sandun.vidusankha@nexeyo.com>
2024-03-20 20:57:30 +0530

    Initial Commit

---
+++ IosCW/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IosCW
+//
+//  Created by Naveen Dhananjaya on 2024-03-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IosCW/IosCWApp.swift
@@ -0,0 +1,17 @@
+//
+//  IosCWApp.swift
+//  IosCW
+//
+//  Created by Naveen Dhananjaya on 2024-03-20.
+//
+
+import SwiftUI
+
+@main
+struct IosCWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
